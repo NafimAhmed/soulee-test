234303ea4e8604793852370a3eb95f7e77ece223
NafimAhmed <recentnafimahmed@gmail.com>
2025-04-27 21:39:17 +0600

    app bar ok

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -10,28 +10,35 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            
-            children: [
-              
-              IconButton(onPressed: (){}, icon: Image.asset('assets/icons/home.png',height: 32,width: 32,)),
-              IconButton(onPressed: (){}, icon: Image.asset('assets/icons/run.png',height: 32,width: 32,)),
-              IconButton(onPressed: (){}, icon: Image.asset('assets/icons/round.png',height: 32,width: 32,)),
-              IconButton(onPressed: (){}, icon: Image.asset('assets/icons/chat.png',height: 32,width: 32,)),
-              IconButton(onPressed: (){}, icon: Image.asset('assets/icons/notification.png',height: 32,width: 32,)),
-              IconButton(onPressed: (){}, icon: Image.asset('assets/icons/more.png',height: 32,width: 32,)),
-            ],
-
-          )
-
+        leading: Container(
+          margin: EdgeInsets.only(left: 16),
+            child: Image.asset('assets/icons/logo.png',height: 35,width: 76,)),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.keyboard_arrow_down))
         ],
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+              children: [
+
+                IconButton(onPressed: (){}, icon: Image.asset('assets/icons/home.png',height: 32,width: 32,)),
+                IconButton(onPressed: (){}, icon: Image.asset('assets/icons/run.png',height: 32,width: 32,)),
+                IconButton(onPressed: (){}, icon: Image.asset('assets/icons/round.png',height: 32,width: 32,)),
+                IconButton(onPressed: (){}, icon: Image.asset('assets/icons/chat.png',height: 32,width: 32,)),
+                IconButton(onPressed: (){}, icon: Image.asset('assets/icons/notification.png',height: 32,width: 32,)),
+                IconButton(onPressed: (){}, icon: Image.asset('assets/icons/more.png',height: 32,width: 32,)),
+              ],
+
+            )
+
+          ],
+        ),
       )
     );
   }
